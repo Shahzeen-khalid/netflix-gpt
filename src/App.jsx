c7ad6f0aedbb910cadfe2ad75fe3f186273d0a22
Shahzeen Pro <apple@Shahzeen-Khalid.local>
2024-10-29 16:34:57 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+     <div className='text-3xl font-bold text-indigo-800'>Shahzeen Khalid</div>
+    </>
+  )
+}
+
+export default App
